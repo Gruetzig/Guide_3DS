5f43fc8f31500ed018ce455f93d783148456118c
Gruetzig <88926852+Gruetzig@users.noreply.github.com>
2023-11-17 11:03:47 +0000

    Deploying to gh-pages from @ Gruetzig/Guide_3DS@dc2fd5f22683d87deb8a2c4056edea220b090890 🚀

--- assets/js/selecting.js
+++ assets/js/selecting.js
@@ -33,7 +33,7 @@ function validate_version(major, minor, region, model) {
 
 // Soundhax
 // 1.0-11.3
-function can_soundhax(major, minor) {
+function can_soundhax(major, minor, native, region, model) {
     let do_redirect = false;
     if(major <= 10) do_redirect = true;
     else if(major == 11 && minor <= 3) do_redirect = true;
@@ -45,9 +45,71 @@ function can_soundhax(major, minor) {
     return false;
 }
 
-// Soundhax
+function can_ssloth(major, minor, native, region, model) {
+    let do_redirect = false;
+    if(major == 11) {
+        if(["U", "E", "J"].includes(region)) {
+            if
+                (
+                (minor == 4 && native == 37) ||
+                (minor == 5 && native == 38) ||
+                (minor == 6 && native == 39) ||
+                (minor == 7 && native == 40) ||
+                (minor == 8 && native == 41) ||
+                (minor == 9 && native == 42) ||
+                (minor == 10 && native == 43) ||
+                (minor == 11 && native == 43) ||
+                (minor == 12 && native == 44) ||
+                (minor == 13 && native == 45)
+                ) {
+                    do_redirect = true;
+            }
+        } else if (region == "K") {
+            if
+                (
+                (model == DEVICE_N3DS && minor == 4 && native == 33) ||
+                (model == DEVICE_N3DS && minor == 5 && native == 34) ||
+                (model == DEVICE_N3DS && minor == 6 && native == 35) ||
+                (model == DEVICE_N3DS && minor == 7 && native == 35) ||
+                (model == DEVICE_N3DS && minor == 8 && native == 35) ||
+                (minor == 9 && native == 36) ||
+                (minor == 10 && native == 37) ||
+                (minor == 12 && native == 38) ||
+                (minor == 13 && native == 39)
+                ) {
+                    do_redirect = true;
+            }
+        }
+    }
+
+    if(do_redirect) {
+        window.location.href = "installing-boot9strap-(ssloth-browser)";
+        return true;
+    }
+    return false;
+}
+
+// safecerthax
+// O3DS only, all regions
+// Works on 1.0 to 11.14
+// Soundhax and SSLoth should be validated before this
+function can_safecerthax(major, minor, native, region, model) {
+    let do_redirect = false;
+    if (model == DEVICE_O3DS) {
+        if (major <= 10) do_redirect = true;
+        else if (major == 11 && minor <= 14) do_redirect = true;
+    }
+
+    if(do_redirect) {
+        window.location.href = "installing-boot9strap-(safecerthax)";
+        return true;
+    }
+    return false;
+}
+
+// MSET9
 // 11.4-11.17
-function can_mset9(major, minor) {
+function can_mset9(major, minor, native, region, model) {
 
     // Exploit supports 11.4 or later
     if(major == 11 && minor >= 4) {
@@ -63,17 +125,34 @@ function can_mset9(major, minor) {
 function redirect() {
     const major = document.getElementById("major").value;
     const minor = document.getElementById("minor").value;
+    const nver = document.getElementById("nver").value;
+    const region = document.getElementById("region").value;
+    const isN3DS = document.getElementById("new3DS").checked;
+    const isO3DS = document.getElementById("old3DS").checked;
+    document.getElementById("result_noneSelected").style.display = "none";
     document.getElementById("result_invalidVersion").style.display = "none";
+    document.getElementById("result_methodUnavailable").style.display = "none";
+    if ((!isN3DS) && (!isO3DS)) {
+        document.getElementById("result_noneSelected").style.display = "block";
+        return;
+    }
 
-    if (!validate_version(major, minor)) {
+    // Realistically only one of these should be possible with the given elements
+    let model = -1;
+    if(isO3DS) model = DEVICE_O3DS
+    else if(isN3DS) model = DEVICE_N3DS;
+
+    if (!validate_version(major, minor, nver, region, model)) {
         document.getElementById("result_invalidVersion").style.display = "block";
         return;
     }
 
     const redirected = [
       can_soundhax,
+      can_ssloth,
+      can_safecerthax,
       can_mset9
-    ].some(func => func(major, minor));
+    ].some(func => func(major, minor, nver, region, model));
     if (redirected) return true;
 
     // if it actually got to this point, there is no exploit available.
